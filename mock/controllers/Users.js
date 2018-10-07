5d48ec0c5eb79d676531f66d9c1a427404d25958
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-10-07 12:49:36 +0900

    [Fix] モックの修正
    POST、DELETEのステータスコード

--- mock/controllers/Users.js
+++ mock/controllers/Users.js
@@ -7,7 +7,7 @@ module.exports.createUser = function createUser (req, res, next) {
   var body = req.swagger.params['body'].value;
   Users.createUser(body)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 201);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
@@ -18,7 +18,7 @@ module.exports.deleteUser = function deleteUser (req, res, next) {
   var user_id = req.swagger.params['user_id'].value;
   Users.deleteUser(user_id)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 204);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
